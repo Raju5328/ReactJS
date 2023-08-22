18da4c3c5698d8c19c1fc1e7c828387c3c057df8
Raju5328 <tummaraju1321@gmail.com>
2023-08-22 20:07:35 +0530

    add project files

---
+++ EPISODE-1/App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1", {id:"heading"}, "Hello Welcome to React");
+
+//console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
